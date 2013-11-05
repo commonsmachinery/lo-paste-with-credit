2fdc9a4424d5b8f891efda5d468be13d7ee4723c
Artem Popov <artfwo@gmail.com>
2013-11-05 10:54:16 +0700

    Paste credit as caption (still not styled).

--- cm-paste.py
+++ cm-paste.py
@@ -14,8 +14,12 @@ from xml.dom import minidom
 import uno
 import unohelper
 
+from com.sun.star.awt import Size
 from com.sun.star.task import XJobExecutor
+from com.sun.star.beans import PropertyValue
 from com.sun.star.text.ControlCharacter import PARAGRAPH_BREAK
+from com.sun.star.text.TextContentAnchorType import AS_CHARACTER
+from com.sun.star.text.TextContentAnchorType import AT_PARAGRAPH
 
 class LOCreditFormatter(libcredit.CreditFormatter):
     """
@@ -85,6 +89,9 @@ class PasteJob(unohelper.Base, XJobExecutor):
             rdf_clip = next(d for d in data_flavors if d.MimeType == "application/rdf+xml")
             rdf = clip.getContents().getTransferData(rdf_clip).value.decode('utf-16')
 
+            img_clip = next(d for d in data_flavors if d.MimeType == "image/png")
+            img_byteseq = clip.getContents().getTransferData(img_clip)
+
             credit = libcredit.Credit(rdf)
 
             # access the current writer document
@@ -95,6 +102,7 @@ class PasteJob(unohelper.Base, XJobExecutor):
             controller = model.getCurrentController()
             text = model.Text
 
+            """
             # paste image - hack, the api should be used here instead of .uno
             dispatch_helper = self.ctx.ServiceManager.createInstance(
                 "com.sun.star.frame.DispatchHelper"); 
@@ -105,6 +113,7 @@ class PasteJob(unohelper.Base, XJobExecutor):
             graphics_access = model.getGraphicObjects()
             last_name = graphics_access.getElementNames()[-1]
             last_obj = graphics_access.getByName(last_name)
+            """
 
             # create the caption in the paragraph after image
             #cursor = text.createTextCursorByRange(last_obj.getAnchor())
@@ -114,8 +123,56 @@ class PasteJob(unohelper.Base, XJobExecutor):
             cursor.gotoStartOfSentence(False)
             cursor.gotoEndOfSentence(True)
 
-            credit_writer = LOCreditFormatter(text, cursor)
+            #credit_writer = LOCreditFormatter(text, cursor)
+            #credit.format(credit_writer)
+
+            text_frame = model.createInstance("com.sun.star.text.TextFrame")
+            text_frame.setSize( Size(15000,400))
+            text_frame.setPropertyValue("AnchorType", AT_PARAGRAPH)
+
+            text.insertTextContent(cursor, text_frame, 0)
+            frame_text = text_frame.getText()
+
+            # create the image directly from clipboard data instead of .uno:Paste
+            img_stream = self.ctx.ServiceManager.createInstanceWithContext(
+                "com.sun.star.io.SequenceInputStream", self.ctx)
+            img_stream.initialize((img_byteseq,))
+            graphic_provider = self.ctx.ServiceManager.createInstanceWithContext(
+                "com.sun.star.graphic.GraphicProvider", self.ctx)
+
+            stream_property = PropertyValue()
+            stream_property.Name = "InputStream"
+            stream_property.Value = img_stream
+
+            graphic_descriptor = graphic_provider.queryGraphicDescriptor((stream_property,))
+            graphic = graphic_provider.queryGraphic((stream_property,))
+            size = graphic_descriptor.getPropertyValue("SizePixel")
+
+            image = model.createInstance('com.sun.star.text.TextGraphicObject')
+            image.setPropertyValue("Graphic", graphic)
+            # a hack to enlarge the tiny pasted images
+            image.setPropertyValue("Width", size.Width * 20)
+            image.setPropertyValue("Height", size.Height * 20)
+
+            cursor = frame_text.createTextCursor() # FIXME: should use the real cursor here
+            frame_text.insertTextContent(cursor, image, False)
+            cursor.gotoNextParagraph(False)
+
+            credit_writer = LOCreditFormatter(frame_text, cursor)
             credit.format(credit_writer)
+
+            image.setPropertyValue("RelativeWidth", 100)
+            #image.setPropertyValue("RelativeHeight", 100)
+            image.setPropertyValue("IsSyncHeightToWidth", True)
+
+            # set image description
+            credit_writer = libcredit.TextCreditFormatter()
+            credit.format(credit_writer, source_depth=0)
+            image.setPropertyValue("Title", credit_writer.get_text())
+
+            credit_writer = libcredit.TextCreditFormatter()
+            credit.format(credit_writer)
+            image.setPropertyValue("Description", credit_writer.get_text())
         else:
             # access the current writer document
             desktop = self.ctx.ServiceManager.createInstanceWithContext(
